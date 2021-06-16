34cf80f554591d0fbb85fc397ce142a6b9ee35da
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-16 08:30:28 -0300

    .

--- api.js
+++ api.js
@@ -3,15 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/cursoreact',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// Ligar á B.D.: 'test'->user da BD, ´nnn´->pass
+mongoose.connect(‘mongodb+srv://test:nnn@nodejscluster-art2k.mongodb.net/test?retryWrites=true’);
+// Confirma ligação na consola
+mongoose.connection.on(‘connected’, function () {
+  console.log(‘Connected to Database ‘+’test’);
 });
-const UserModel  = require('./src/models/user');
-var USERS = [
-    { 'id': 1, 'username': 'brunohauck', 'password': '123456' },
-    { 'id': 2, 'username': 'paul' , 'password': '123456' },
-];
+// Mensagem de Erro
+mongoose.connection.on(‘error’, (err) => {
+  console.log(‘Database error ‘+err);
+});
 
 var HELLO = [
     { 'msg': 'Hello Express' }
